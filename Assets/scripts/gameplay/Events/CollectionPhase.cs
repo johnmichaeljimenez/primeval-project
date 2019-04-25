5496461a485e9f6ed2e6f788ceda50a54288857b
johnmichaeljimenez <johnjimenezdisgorge@gmail.com>
2019-04-25 23:38:20 +0800

    timed event

---
+++ Assets/scripts/gameplay/Events/CollectionPhase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Primeval.PlayerCharacter;
+using Mirror;
+
+namespace Primeval.Events
+{
+    public class CollectionPhase : TimedEventBase
+    {
+        public string notificationText;
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        public override void OnSecondTick()
+        {
+            base.OnSecondTick();
+        }
+    }
+}
