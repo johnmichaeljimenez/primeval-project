2b35526df9145de17bfeb987ba7644c921d72ac4
johnmichaeljimenez <johnjimenezdisgorge@gmail.com>
2019-04-29 00:40:38 +0800

    multiplayer sound

--- Assets/scripts/gameplay/PlayerCharacter/AudioPlayer.cs
+++ Assets/scripts/gameplay/PlayerCharacter/AudioPlayer.cs
@@ -8,6 +8,8 @@ namespace Primeval.PlayerCharacter
 {
     public class AudioPlayer : PlayerModuleBase
     {
+        public AudioClip[] soundBank; //multiplayer only
+        
         public AudioSource source;
 
         public override void Initialize()
@@ -20,9 +22,33 @@ namespace Primeval.PlayerCharacter
             base.OnUpdate();
         }
 
-        public void PlaySound(AudioClip clip)
+        public void PlaySound(AudioClip clip, bool networked = false)
         {
+            if (networked && isLocalPlayer)
+            {
+                CmdPlay(clip.name);
+                return;
+            }
             source.PlayOneShot(clip);
+        }
+
+        [Command]
+        public void CmdPlay(string n)
+        {
+            RpcPlay(n);
+        }
+
+        [ClientRpc]
+        public void RpcPlay(string n)
+        {
+            foreach (AudioClip i in soundBank)
+            {
+                if (i.name.ToLower() == n.ToLower())
+                {
+                    source.PlayOneShot(i);
+                    break;
+                }
+            }
         }
     }
 }
