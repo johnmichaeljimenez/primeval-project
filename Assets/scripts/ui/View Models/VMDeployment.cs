03d19be2ad4699b2d702469d445e62cd3eaf8732
johnmichaeljimenez <johnjimenezdisgorge@gmail.com>
2019-04-28 16:44:12 +0800

    deployment data bind

---
+++ Assets/scripts/ui/View Models/VMDeployment.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.ComponentModel;
+using UnityWeld.Binding;
+
+namespace Primeval.ViewModels
+{
+    [Binding]
+    public class VMDeployment : GenericSingletonClass<VMDeployment>, INotifyPropertyChanged
+    {
+        //altitude
+        //target height
+        //starting height
+
+
+        private string text = "<Type some text>";
+
+        [Binding]
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                if (text == value)
+                {
+                    return; // No change.
+                }
+
+                text = value;
+
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
