c33d9b0c3ba7b451ecc1e5ecdbaa8b8583bc7be4
John Jimenez <johnjimenezdisgorge@gmail.com>
2019-05-28 16:14:34 +0800

    message dialog

--- Assets/scripts/ui/MessageDialog.cs
+++ Assets/scripts/ui/MessageDialog.cs
@@ -1,8 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using Primeval.ViewModels;
 
 public class MessageDialog : MonoBehaviour
 {
-    
+    public Button okButton, cancelButton;
+    public void Show(string content, string caption = "", UnityAction okAction = null, bool okOnly = true, UnityAction cancelAction = null)
+    {
+        VMMessageBox vm = GetComponent<VMMessageBox>();
+        vm.Content = content;
+        vm.Caption = caption;
+
+        okButton.onClick.RemoveAllListeners();
+        cancelButton.onClick.RemoveAllListeners();
+
+        cancelButton.gameObject.SetActive(!okOnly);
+        if (!okOnly)
+        {
+            if (cancelAction != null)
+                cancelButton.onClick.AddListener(cancelAction);
+            cancelButton.onClick.AddListener(Hide);
+        }
+
+        if (okAction != null)
+            okButton.onClick.AddListener(okAction);
+        okButton.onClick.AddListener(Hide);
+
+        gameObject.SetActive(true);
+    }
+
+    void Hide()
+    {
+        gameObject.SetActive(false);
+    }
 }
